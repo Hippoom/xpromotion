1bec4e7f1cf8135939e4dfba90d3f247a0549b7a
Hippoom <hippoom@gmail.com>
2014-04-18 10:07:43 +0800

    Organize EventHandling with InstanceMethods And ClassMethods

--- promotion-ruby/src/main/cqrs/event_handling.rb
+++ promotion-ruby/src/main/cqrs/event_handling.rb
@@ -19,26 +19,40 @@ end
 
 module EventHandling
   module Dsl
-    
-    def handle_event event
-      handler = self.class.event_handler event
-      instance_exec(event, &handler)#shift context
+    module InstanceMethods
+      
+      def handle_event event
+        handler = event_handler_for event
+        instance_exec(event, &handler)#shift context as instance variables in blocks are bounded with class
+      end
+
+      def event_handler_for event
+        self.class.send(:event_handler_for, event)
+      end
+
+      private  :handle_event, :event_handler_for
     end
 
-    private  :handle_event
+    module ClassMethods
+      def on(event_type, &handler)
+        @event_handlers[event_type] = handler
+      end
+
+      def event_handler_for event
+        @event_handlers[event.class]
+      end
+
+      private :on, :event_handler_for
+    end
 
     def self.included(clazz)
+
       clazz.class_eval do
-        @event_handlers = {}
-
-        def self.on(event_clazz, &handler)
-          @event_handlers[event_clazz] = handler
-        end
-
-        def self.event_handler event
-          @event_handlers[event.class]
-        end
+        include InstanceMethods
+        extend ClassMethods
+        @event_handlers = {}#init empty Hash for ClassMethods
       end
+
     end
   end
 end
